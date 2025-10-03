af7470f766907527597a2cd8ee565c506cc3d1d7
Sayiful14 <sayifullahipul92@gmail.com>
2025-10-03 11:21:00 +0700

    Instalasi, konfigurasi dan halaman utama

---
+++ LaraPress/resources/views/kontak.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kontak - LaraPress</title>
+</head>
+<body>
+    <h1>Kontak LaraPress</h1>
+    <p>Halaman Kontak.</p>
+    <p>Nama : Mohammad Sayifullah</p>
+    <p>Npm : 4523210066</p>
+    <p>Email : sayifullahipul92@gmail.com</p>
+    <p>Nomor Hp : 081574749192</p>
+    <a href="/tentang-kami">Lihat Halaman Tentang Kami</a>
+    <br>
+    <a href="/">Kembali ke Halaman Utama</a>
+</body>
+</html>
